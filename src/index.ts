7fdf0051567e756e395d761379716b8789e20ff4
Darren Griffin <darren.griffin@live.co.uk>
2024-10-28 18:03:24 +0000

    Added wrangler for mobile screenshot service

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import puppeteer from '@cloudflare/puppeteer';
+
+interface Env {
+	MYBROWSER: Fetcher;
+	SCREENSHOT_KV: KVNamespace;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		const { searchParams } = new URL(request.url);
+		let url = searchParams.get('url');
+		let img: Buffer;
+		if (url) {
+			url = new URL(url).toString(); // normalize
+			img = (await env.SCREENSHOT_KV.get(url, { type: 'arrayBuffer' })) as Buffer;
+			
+			if (img === null) {
+				const browser = await puppeteer.launch(env.MYBROWSER);
+				const page = await browser.newPage();
+
+				page.setViewport({ width: 400, height: 800 });
+				await page.goto(url);
+				page.addStyleTag({
+					content: `
+						html, body {
+							overflow: hidden;
+						}
+					`,
+				});
+				await new Promise((resolve) => setTimeout(resolve, 5000));
+				img = (await page.screenshot()) as Buffer;
+				await env.SCREENSHOT_KV.put(url, img, {
+					expirationTtl: 60 * 60 * 24,
+				});
+				await browser.close();
+			}
+
+			return new Response(img, {
+				headers: {
+					'content-type': 'image/jpeg',
+				},
+			});
+		} else {
+			return new Response('Please add an ?url=https://example.com/ parameter');
+		}
 	},
 } satisfies ExportedHandler<Env>;
